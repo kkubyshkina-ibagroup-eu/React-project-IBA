9f8af377a7951968651275f33f8ffe31b1bf59cc
Katie <KKubyshkina@gomel.iba.by>
2022-03-23 22:45:32 +0300

    Task 2.1

---
+++ my-first-project/src/components/Card.js
@@ -0,0 +1,17 @@
+import "./Card.css";
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="card-header">
+        <h1> Info </h1>
+      </div>
+      <h1>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </h1>
+    </div>
+  );
+}
+
+export default Card;
