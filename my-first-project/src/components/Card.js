6ac1fdb4c4e535b8cc2b7b0b3c2b16e42ac3ff03
Katie <KKubyshkina@gomel.iba.by>
2022-05-09 22:06:31 +0300

    Split component into CardHeader and CardBody

--- my-first-project/src/components/Card.js
+++ my-first-project/src/components/Card.js
@@ -1,30 +1,13 @@
 import "./Card.css";
 import React, { useState } from "react";
-import { BsPencilSquare } from "react-icons/bs";
-import { BsCheckLg } from "react-icons/bs";
-import { FcCancel } from "react-icons/fc";
+import CardBody from "./CardBody";
+import CardHeader from "./CardHeader";
 
 function Card(props) {
   const { cardInfo, viewOnly, cardUpdateHandler } = props;
 
-  const cardCheckboxHandler = () => {
-    cardUpdateHandler({ ...cardInfo, checked: !cardInfo.checked });
-  };
-
   const [unsavedTitle, setUnsavedTitle] = useState();
   const [unsavedText, setUnsavedText] = useState();
-
-  const clickPencil = () => {
-    cardUpdateHandler({ ...cardInfo, isEdeting: true, checked: false });
-  };
-
-  const userInputTitle = (event) => {
-    setUnsavedTitle(event.target.value);
-  };
-
-  const userInputText = (event) => {
-    setUnsavedText(event.target.value);
-  };
 
   const clickSave = () => {
     if (unsavedTitle) {
@@ -51,61 +34,16 @@ function Card(props) {
     }
   };
 
-  const clickCancel = () => {
-    cardUpdateHandler({ ...cardInfo, isEdeting: false });
-  };
-
   return (
-    <div className={cardInfo.checked ? "checked-card" : "card"}>
-      {cardInfo.isEdeting ? (
-        <div>
-          <input
-            className="title-form"
-            type="text"
-            defaultValue={cardInfo.title}
-            onChange={userInputTitle}
-          />
-          <button className="cancel-button-edit" onClick={clickCancel}>
-            <FcCancel size={22} />
-          </button>
-          <button className="save-button-edit" onClick={clickSave}>
-            <BsCheckLg size={20} />
-          </button>
-          <textarea
-            className="text-form "
-            type="text"
-            defaultValue={cardInfo.text}
-            onChange={userInputText}
-          />
-        </div>
-      ) : (
-        <div>
-          <div className="card-header">
-            <input
-              className="card-checkbox"
-              type="checkbox"
-              onChange={cardCheckboxHandler}
-            />
-            {cardInfo.title}
-            {!viewOnly ? (
-              <div>
-                <button className="pencil-button" onClick={clickPencil}>
-                  <BsPencilSquare size={20} />
-                </button>
-                <button className="cancel-button" onClick={clickCancel}>
-                  <FcCancel size={22} />
-                </button>
-                <button className="save-button" onClick={clickSave}>
-                  <BsCheckLg size={20} />
-                </button>
-              </div>
-            ) : (
-              ""
-            )}
-          </div>
-          <h1>{cardInfo.text}</h1>
-        </div>
-      )}
+    <div className={`card ${cardInfo.checked ? "checked" : ""}`}>
+      <CardHeader
+        cardInfo={cardInfo}
+        viewOnly={viewOnly}
+        cardUpdateHandler={cardUpdateHandler}
+        clickSave={clickSave}
+        setUnsavedTitle={setUnsavedTitle}
+      />
+      <CardBody cardInfo={cardInfo} setUnsavedText={setUnsavedText} />
     </div>
   );
 }
