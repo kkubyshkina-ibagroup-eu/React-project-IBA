e3d45ff6d1a046a4757a791f85e7226c1d93ff0b
Katie <KKubyshkina@gomel.iba.by>
2022-05-23 15:50:54 +0300

    Create counter and aplaying context logic

--- my-first-project/src/components/Header.js
+++ my-first-project/src/components/Header.js
@@ -1,11 +1,21 @@
-import "./Header.css"
+import React, { useContext } from "react";
+import CardContext from "../contex/contex";
+import "./Header.css";
 
 function Header() {
-    return (
-        <div className='header'>
-         <h1> My first application! </h1>
-        </div>
-      );
+  const ctx = useContext(CardContext);
+
+  return (
+    <div className="header">
+      <h1> My first application! </h1>
+      {
+        <button className="counter">
+          Cards
+          <span className="counter-number">{ctx.cardState.length}</span>
+        </button>
+      }
+    </div>
+  );
 }
 
 export default Header;
