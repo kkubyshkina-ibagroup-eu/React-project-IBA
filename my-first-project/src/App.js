d3691623ecf1e6c392ca90a0c607a9579816e54e
Katie <KKubyshkina@gomel.iba.by>
2022-06-01 11:49:53 +0300

    Add routing

--- my-first-project/src/App.js
+++ my-first-project/src/App.js
@@ -4,12 +4,15 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import CardList from "./components/CardList";
 import CardContext from "./contex/contex";
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./components/NotFound";
+import SignInForm from "./components/SignInForm";
 
 const ViewOnlyCheckbox = styled.input`
   accent-color: #651679;
-  position: absolute;
+  position: relative;
   left: 150px;
-  top: 160px;
+  bottom: 10px;
   width: 30px;
   height: 30px;
 `;
@@ -17,7 +20,7 @@ const ViewOnlyCheckbox = styled.input`
 const DeleteButton = styled.button`
   position: absolute;
   left: 200px;
-  top: 160px;
+  top: 200px;
   width: 120px;
   height: 60px;
   background-color: #651679;
@@ -33,7 +36,7 @@ const DeleteButton = styled.button`
 const AddCard = styled.button`
   position: absolute;
   left: 330px;
-  top: 160px;
+  top: 200px;
   width: 120px;
   height: 60px;
   background-color: #651679;
@@ -50,13 +53,27 @@ function App() {
   return (
     <React.Fragment>
       <Header />
-      <div className="view-only-header">View only</div>
-      <ViewOnlyCheckbox type="checkbox" onChange={ctx.viewOnlyHandler} />
-      <DeleteButton onClick={ctx.deletedHandler}>
-        Delete selected cards
-      </DeleteButton>
-      <AddCard onClick={ctx.addCardHandler}> Add card</AddCard>
-      <CardList />
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <main>
+              <div className="view-only-header">View only</div>
+              <ViewOnlyCheckbox
+                type="checkbox"
+                onChange={ctx.viewOnlyHandler}
+              />
+              <DeleteButton onClick={ctx.deletedHandler}>
+                Delete selected cards
+              </DeleteButton>
+              <AddCard onClick={ctx.addCardHandler}> Add card</AddCard>
+              <CardList />
+            </main>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/sign-in" element={<SignInForm />} />
+      </Routes>
     </React.Fragment>
   );
 }
--- my-first-project/src/index.js
+++ my-first-project/src/index.js
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { CardContextProvider } from "./contex/contex";
 
 ReactDOM.render(
-  <CardContextProvider>
-    <App />
-  </CardContextProvider>,
+  <BrowserRouter>
+    <CardContextProvider>
+      <App />
+    </CardContextProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
