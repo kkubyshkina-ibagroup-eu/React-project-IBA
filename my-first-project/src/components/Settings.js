a661cf253d9a92314934a5aad116259bd3ec7a91
Katie <KKubyshkina@gomel.iba.by>
2022-06-20 16:15:16 +0300

    Create new page with viewOnly checkbox

---
+++ my-first-project/src/components/Settings.js
@@ -0,0 +1,33 @@
+import "./NotFound.css";
+import styled from "styled-components";
+import { useDispatch } from "react-redux/es/exports";
+import { cardsActions } from "../store/cardsSlice";
+import { Fragment } from "react";
+
+const Settings = () => {
+  const ViewOnlyCheckbox = styled.input`
+    accent-color: #651679;
+    position: relative;
+    left: 150px;
+    bottom: 10px;
+    width: 30px;
+    height: 30px;
+  `;
+  const dispatch = useDispatch();
+  return (
+    <Fragment>
+      <div className="view-only-header">View only</div>
+      <ViewOnlyCheckbox
+        type="checkbox"
+        onChange={() => {
+          dispatch(cardsActions.setViewOnly());
+        }}
+      />
+      <div className="not-found-page">
+        <h1>Settings</h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Settings;
