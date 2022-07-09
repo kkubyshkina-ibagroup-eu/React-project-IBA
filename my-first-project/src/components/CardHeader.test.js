2222f228bdc42da2cef386e16e3e0d106bf9d80c
Katie <KKubyshkina@gomel.iba.by>
2022-07-09 21:56:12 +0300

    Add more tests

--- my-first-project/src/components/CardHeader.test.js
+++ my-first-project/src/components/CardHeader.test.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import CardHeader from "./CardHeader";
 import CardBody from "./CardBody";
 import { render, screen, fireEvent } from "./customRender";
+import Settings from "./Settings";
 
 describe("CardHeader component", () => {
   test("render correct CardHeader component items if isEditable mode is on and test save button", () => {
@@ -13,20 +14,19 @@ describe("CardHeader component", () => {
         <CardBody cardInfo={{ isEdeting: true }} />
       </BrowserRouter>
     );
-    const inputTitleField = screen.getByTestId("input-title-field");
     const inputTextField = screen.getByTestId("input-text-field");
+    const inputTitleField = screen.getByTestId("input-title-field");
     const saveBtn = screen.getByTestId("save-button-edit");
     const cancelBtn = screen.getByTestId("cancel-button-edit");
     userEvent.click(cancelBtn);
-    fireEvent.change(inputTitleField, { target: { value: "bla" } });
+    fireEvent.change(inputTitleField, { target: { value: "lala" } });
     userEvent.click(saveBtn);
-    expect(inputTitleField.value).toBe("bla");
-    const cardCheckbox = screen.queryByTestId("card-checbox");
-    expect(cardCheckbox).not.toBeInTheDocument();
+    expect(inputTitleField.value).toBe("lala");
     fireEvent.change(inputTextField, { target: { value: "bla" } });
     userEvent.click(saveBtn);
     expect(inputTextField.value).toBe("bla");
-    expect(inputTitleField).toBeInTheDocument();
+    const cardCheckbox = screen.queryByTestId("card-checbox");
+    expect(cardCheckbox).not.toBeInTheDocument();
     expect(saveBtn).toBeInTheDocument();
     expect(cancelBtn).toBeInTheDocument();
     expect(screen.queryByTestId("pencil-button")).not.toBeInTheDocument();
@@ -60,5 +60,21 @@ describe("CardHeader component", () => {
     const cardHeader = screen.getByTestId("card-header");
     expect(cardHeader).toBeInTheDocument();
     expect(cardHeader).toHaveClass("card-header checked");
+  });
+
+  test("View Only mode", () => {
+    render(
+      <BrowserRouter>
+        <CardHeader cardInfo={{ isEdeting: false }} />
+        <Settings />
+      </BrowserRouter>
+    );
+    const viewOnlyCheckbox = screen.getByTestId("view-only-checkbox");
+    const pencilIcon = screen.getByTestId("pencil-button");
+    expect(viewOnlyCheckbox).toBeInTheDocument();
+    expect(viewOnlyCheckbox).not.toBeChecked();
+    userEvent.click(viewOnlyCheckbox);
+    expect(viewOnlyCheckbox).toBeChecked();
+    expect(pencilIcon).not.toBeInTheDocument();
   });
 });
